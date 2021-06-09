0d6dd36b1290b5d1a31bb56c689370c356e95092
Liping_Yin <liping.yin1221@gmail.com>
2021-06-09 12:07:49 +0200

    3d card animation

---
+++ Animations/js/3d-shoes/index.js
@@ -0,0 +1,6 @@
+// Movement animation
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+// Moving Animation Event
+container.addEventListener("mousemove", (e) => {});
