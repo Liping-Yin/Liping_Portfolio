6ca54e9c7ad36d04b9090af4bd34a3313fc113f1
Liping_Yin <liping.yin1221@gmail.com>
2021-06-28 22:23:20 +0200

    canvas particles animation

--- Projects/animations/canvas/first-tutorial/index.js
+++ Projects/animations/canvas/first-tutorial/index.js
@@ -1,31 +1,119 @@
 "use strict";
-// var body = document.getElementsByTagName("body")[0];
+
 const canvas = document.getElementById("myCanvas");
-let ctx;
-// checking for browser support
-window.addEventListener("load", draw);
+const ctx = canvas.getContext("2d");
 
-function draw() {
-  console.log("draw");
-  if (canvas.getContext) {
-    ctx = canvas.getContext("2d");
-    //   drawing codes
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+let particleArray = [];
+const colours = [
+  "white",
+  "rgba(255,255,255,.3)",
+  "rgba(173,216,230,.8)",
+  "rgba(211,211,211,.8)",
+];
+const maxSize = 40;
+const minSize = 0;
+const mouseRadius = 60;
+//mouse position
+let mouse = {
+  x: null,
+  y: null,
+};
 
-    ctx.fillStyle = "rgba(0,0,200,.5)";
+window.addEventListener("mouseover", function (e) {
+  mouse.x = e.x;
+  mouse.y = e.y;
+  console.log(mouse);
+});
 
-    // DRAW RECTANGLE
-    ctx.fillRect(25, 25, 100, 100);
-    ctx.clearRect(50, 50, 50, 50);
-    ctx.strokeRect(55, 55, 40, 40);
+// Creat constructor function for particles
+function Particle(x, y, directionX, directionY, size, colour) {
+  this.x = x;
+  this.y = y;
+  this.directionX = directionX;
+  this.directionY = directionY;
+  this.size = size;
+  this.colour = colour;
+}
 
-    // DRAW BY PATHS
-    ctx.beginPath(); //path is created
-    ctx.moveTo(200, 200); // move the pen to spot (x,y)
-    ctx.lineTo(225, 175);
-    ctx.lineTo(225, 220);
-    ctx.fill();
-  } else {
-    // fallback content: canvas-unspported code
-    canvas.innerText = "a clock";
+// add draw method to prototype
+Particle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+  ctx.fillStyle = this.colour;
+  ctx.fill();
+};
+
+// add update method to particle prototype
+Particle.prototype.update = function () {
+  if (this.x + this.size * 2 > canvas.width || this.x - this.size * 2 < 0) {
+    this.directionX = -this.directionX;
+  }
+  if (this.y + this.size * 2 > canvas.height || this.y - this.size * 2 < 0) {
+    this.directionY = -this.directionY;
+  }
+  this.x += this.directionX;
+  this.y += this.directionY;
+
+  //mouse interactivety
+
+  // check if mouse and particle are close
+  if (
+    mouse.x - this.x < mouseRadius &&
+    mouse.x - this.x > -mouseRadius &&
+    mouse.y - this.y < mouseRadius &&
+    mouse.y - this.y > -mouseRadius
+  ) {
+    if (this.size < maxSize) {
+      this.size += 3;
+    }
+  } else if (this.size > minSize) {
+    this.size -= 1;
+  }
+  if (this.size < 0) {
+    this.size = 0;
+  }
+  this.draw();
+};
+
+// Create particle array
+function init() {
+  particleArray = [];
+  for (let i = 0; i < 1000; i++) {
+    let size = 0;
+    let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
+    let y = Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
+    let directionX = Math.random() * 0.2 - 0.1;
+    let directionY = Math.random() * 0.2 - 0.1;
+    let colour = colours[Math.floor(Math.random() * colours.length)];
+
+    particleArray.push(
+      new Particle(x, y, directionX, directionY, size, colour)
+    );
   }
 }
+// animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+  }
+}
+init();
+animate();
+
+// resize event
+window.addEventListener("resize", function () {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  init();
+});
+
+// remove mouse periodically
+setInterval(function () {
+  mouse.x = undefined;
+  mouse.y = undefined;
+}, 1000);
